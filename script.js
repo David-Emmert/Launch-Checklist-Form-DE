3ee1c9ac00713565df0e097ee3f9bb6d59aa76e9
David Emmert <emmertd30@gmail.com>
2023-01-11 11:41:44 -0500

    added validation

--- script.js
+++ script.js
@@ -11,3 +11,36 @@
 </ul>
 <img src="${}">
 */
+window.addEventListener("load", function() {
+
+   let listedPlanets;
+   let form = document.getElementById("launchForm")
+   form.addEventListener("submit", function(event) {
+    let pilotNameInp = document.getElementById("pilotName")
+    let copilotNameInp = document.getElementById("copilotName")
+    let fuelLevelInp = document.getElementById("fuelLevel")
+    let cargoMassInp = document.getElementById("cargoMass")
+    if (pilotNameInp.value.length < 1 || copilotNameInp.value.length < 1 || fuelLevelInp.value.length < 1 || cargoMassInp.value.length < 1) {
+        alert("All fields required!")
+        event.preventDefault()
+    }
+    else if (!isNaN(pilotNameInp.value) || !isNaN(copilotNameInp.value)) {
+        alert("Numbers are not authorized for names.")
+        event.preventDefault()
+    }
+    else if (isNaN(fuelLevelInp) || isNaN(cargoMassInp)) {
+      alert("Only numbers are authorized for cargo mass and fuel level fields.")
+      event.preventDefault()
+    }
+   })
+      // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   let listedPlanetsResponse;
+   listedPlanetsResponse.then(function (result) {
+       listedPlanets = result;
+       console.log(listedPlanets);
+   }).then(function () {
+       console.log(listedPlanets);
+       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+   })
+
+});
